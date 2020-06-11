11dbc3762d0257216665bd2accd3eb15400d4d1d
Mark Verrips <2083591@talnet.nl>
2020-06-11 14:41:51 +0200

    grafiek klaar

--- studentenlijst-popup.php
+++ studentenlijst-popup.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+
+    <button id="myBtn">voorbeeld van popup</button>
+
+<div id="myModal" class="modal">
+
+
+  <div class="modal-content">
+    <span class="close">&times;</span>
     <?php
+ 
 $dataPoints = array(
 	array("label"=> "9 Schooldagen geleden ", "y"=> 7),
 	array("label"=> "8 Schooldagen geleden ", "y"=> 2),
@@ -61,17 +71,31 @@ chart.render();
 </body>
 </html>  
 
-<form form action="/vragenlijst2.php">
-<label for="vraag">wil je een gesprek met een coach?</label><br>
-<input type="checkbox" name="muzieksoort" value="ja" checked>ja
-<input type="checkbox" name="muzieksoort" value="nee">nee<br>
-<label for="msg">met wie zou je een gesprek willen?</label><br>
-<textarea id="msg" name="user_message"></textarea><br>
-<label for="msg">Wanneer zou je een gesprek willen?</label><br>
-<input type="date" id="start" name="trip-start"
-       value="2020-01-04"
-       min="2020-01-04" max="2020-08-24">
-<input type="submit" value="Verzenden" ><a href="vragenlijststudenten.php"></a>
-</form>
+  </div>
+
+</div>
+<script>
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+  
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  </script>
+
 </body>
 </html>
